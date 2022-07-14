054183fa186c313aca39e497068507282d3df5b8
Тихомир Найденов <tihomirnaydenov02@pmgkk.com>
2022-07-14 14:23:05 +0300

    Rename LogOut.cshtml and LogOut.cshtml.cs

---
+++ LibrarySystem/Areas/Identity/Pages/Account/LogОut.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LibrarySystem.Areas.Identity.Pages.Account;
+
+[AllowAnonymous]
+public class LogOutModel : PageModel
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly ILogger<LogOutModel> _logger;
+
+    public LogOutModel(SignInManager<IdentityUser> signInManager, ILogger<LogOutModel> logger)
+    {
+        _signInManager = signInManager;
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPost(string returnUrl = null)
+    {
+        await _signInManager.SignOutAsync();
+        _logger.LogInformation("User logged out.");
+        if (returnUrl != null)
+        {
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+            return RedirectToPage();
+        }
+    }
+}
